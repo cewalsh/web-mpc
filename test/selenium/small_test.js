092a760cdd3efd9d135c4496f006eecf9acda028
Conor Walsh <55603156+cewalsh@users.noreply.github.com>
2022-04-09 17:42:07 -0400

    Update small_test.js

--- test/selenium/small_test.js
+++ test/selenium/small_test.js
@@ -1,44 +1,22 @@
-// const {By,Key,Builder} = require("selenium-webdriver");
-require("chromedriver");
- 
-async function example(){
- 
-       var searchString = "Automation testing with Selenium";
- 
-      var webdriver = require("selenium-webdriver");
-      var chrome = require("selenium-webdriver/chrome");
+const { Builder } = require('selenium-webdriver')
 
-/** 
- * Set chrome command line options/switches
-*/      
-      var chromeOptions = new chrome.Options();
-      chromeOptions.addArguments("--no-sandbox");
-      chromeOptions.addArguments("test-type");
-      chromeOptions.addArguments("start-maximized");
-      chromeOptions.addArguments("--js-flags=--expose-gc");
-      chromeOptions.addArguments("--enable-precise-memory-info");
-      chromeOptions.addArguments("--disable-popup-blocking");
-      chromeOptions.addArguments("--disable-default-apps");
-      chromeOptions.addArguments("--disable-infobars");
+async function start() {
+  const chrome = require('selenium-webdriver/chrome')
+  const options = new chrome.Options()
 
-driver = new webdriver.Builder()
-             .forBrowser("chrome")
-             .setChromeOptions(chromeOptions)
-             .build();
- 
-        //To fetch http://google.com from the browser with our code.
-        await driver.get("http://google.com");
-            
-        //To send a search query by passing the value in searchString.
-        await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
- 
-        //Verify the page title and print it
-        var title = await driver.getTitle();
-        console.log('Title is:',title);
- 
-        //It is always a safe practice to quit the browser after execution
-        await driver.quit();
- 
+  options.addArguments('--disable-dev-shm-usage')
+  options.addArguments('--no-sandbox')
+
+  const driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build()
+
+  await driver.get('https://google.com')
+  await driver.sleep(1000)
+  const text = await driver.executeScript('return document.documentElement.innerText')
+  console.log(text)
+  driver.quit()
 }
- 
-example()
+
+start()

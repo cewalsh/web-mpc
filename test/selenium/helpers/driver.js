faf6b9fcdf781e50cc032632c20dcc376c9d15f3
Conor Walsh <55603156+cewalsh@users.noreply.github.com>
2022-04-09 18:07:15 -0400

    Update driver.js

--- test/selenium/helpers/driver.js
+++ test/selenium/helpers/driver.js
@@ -10,28 +10,28 @@ module.exports = {
     var service = new seleniumChrome.ServiceBuilder(chromDriver.path).build();
     seleniumChrome.setDefaultService(service);
 
-//     driver = new webDriver.Builder()
-//       .forBrowser('chrome')
-//       .withCapabilities(webDriver.Capabilities.chrome())
-//       .setChromeOptions(new seleniumChrome.Options().setUserPreferences({
-//         'profile.content_settings.exceptions.automatic_downloads.*.setting': 1,  // allow multiple file download chrome >74
-//         'profile.default_content_setting_values.automatic_downloads': 1,  // allow multiple file download chrome >46
-//         'profile.default_content_settings.multiple-automatic-downloads': 1,  // allow multiple file download chrome <46
-//         'download.prompt_for_download': false
-//       })
-//       .addArguments('--remote-debugging-port=9222'))
-//       .build();
-    const chrome = require('selenium-webdriver/chrome')
-    const options = new chrome.Options()
-
-    options.addArguments('--disable-dev-shm-usage')
-    options.addArguments('--no-sandbox')
-    options.addArguments('--headless')
-
-    const driver = new Builder()
+    driver = new webDriver.Builder()
       .forBrowser('chrome')
-      .setChromeOptions(options)
-      .build()
+      .withCapabilities(webDriver.Capabilities.chrome())
+      .setChromeOptions(new seleniumChrome.Options().setUserPreferences({
+        'profile.content_settings.exceptions.automatic_downloads.*.setting': 1,  // allow multiple file download chrome >74
+        'profile.default_content_setting_values.automatic_downloads': 1,  // allow multiple file download chrome >46
+        'profile.default_content_settings.multiple-automatic-downloads': 1,  // allow multiple file download chrome <46
+        'download.prompt_for_download': false
+      })
+      .addArguments('--remote-debugging-port=9222'))
+      .build();
+//     const chrome = require('selenium-webdriver/chrome')
+//     const options = new chrome.Options()
+
+//     options.addArguments('--disable-dev-shm-usage')
+//     options.addArguments('--no-sandbox')
+//     options.addArguments('--headless')
+
+//     const driver = new Builder()
+//       .forBrowser('chrome')
+//       .setChromeOptions(options)
+//       .build()
 
     return driver;
   },
